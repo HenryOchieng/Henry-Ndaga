06bbcbf3292282ad85bb70b946854ef81eafa078
Henry1886 <henryochieng8@gmail.com>
2021-01-30 13:28:13 +0300

    settings file changes

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -139,10 +139,10 @@ USE_TZ = True
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/statifiles/'
-#STATICFILES_DIRS = (
- #   os.path.join(BASE_DIR, 'staticfiles'),
-#)
+STATIC_URL = '/staticfiles/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
