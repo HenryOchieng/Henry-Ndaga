6654ee7a0fa6287e673ed826e7ae660622df783c
Henry1886 <henryochieng8@gmail.com>
2021-02-01 23:36:42 +0300

    installing django_heroku and activating it

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -69,7 +70,7 @@ ROOT_URLCONF = 'Portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -149,6 +150,8 @@ STATICFILES_DIRS = (
 import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
+
+django_heroku.settings(locals())
 #MEDIA_URL = '/media/'
 #MEDIA_URL = os.path.join(BASE_DIR,'media')
 
