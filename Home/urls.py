463415d40e59cdf809929b5d2a035a7e4f76322d
Henry1886 <henryochieng8@gmail.com>
2021-02-03 14:35:05 +0300

    project changes

--- Home/urls.py
+++ Home/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 #from . import HomeView
-from django.views.generic import TemplateView
+#from django.views.generic import TemplateView
 
-#from .import views
+from .import views
 
 urlpatterns = [
-    #path('', views.Home, name="Home"),
-    path('', TemplateView.as_view(template_name = "Home/index.html")),
-    path('About', TemplateView.as_view(template_name = "About/index.html")),
-    #path('About', views.About, name="About"),
-    #path('The_Portfolio', views.The_Portfolio, name="The_Portfolio"),
-    #path('Contacts', views.Contacts, name="Contacts")
+    path('', views.Home, name="Home"),
+    #path('', TemplateView.as_view(template_name = "Home/index.html")),
+    #path('About', TemplateView.as_view(template_name = "About/index.html")),
+    path('About', views.About, name="About"),
+    path('The_Portfolio', views.The_Portfolio, name="The_Portfolio"),
+    path('Contacts', views.Contacts, name="Contacts")
 ]
--- Home/views.py
+++ Home/views.py
@@ -1,24 +1,22 @@
 from django.shortcuts import render
-from django.views.generic.base import TemplateView
+#from django.views.generic.base import TemplateView
 from django.http import HttpResponse
 from .models import Home
 
 # Create your views here.
 
-class HomeView(TemplateView):
-    template_name = "Home/index.html"
-    def Home(self):
-        return render('Home/index.html')
-class AboutView(TemplateView):
-    template_name = "About/index.html"
-#def Home(request):
- #   return render(request,'Home/index.html')
+#class HomeView(TemplateView):
+ #   template_name = "Home/index.html"
+#class AboutView(TemplateView):
+ #   template_name = "About/index.html"
+def Home(request):
+    return render(request,'Home/index.html')
 
-#def About(request):
- #   return render(request, 'About/index.html')
+def About(request):
+    return render(request, 'About/index.html')
 
-#def The_Portfolio(request):
- #   return render(request, 'The_Portfolio/index.html')
+def The_Portfolio(request):
+    return render(request, 'The_Portfolio/index.html')
 
-#def Contacts(request):
- #   return render(request, 'Contacts/index.html')
+def Contacts(request):
+    return render(request, 'Contacts/index.html')
