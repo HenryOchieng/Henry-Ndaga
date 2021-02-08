0a64064266c898974721c65559dff9129e938da3
Henry1886 <henryochieng8@gmail.com>
2021-02-08 12:04:07 +0300

    changes to settings file

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -17,7 +17,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 #TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates'),
-TEMPLATE_DIR = ['Portfolio/templates']
+#TEMPLATE_DIR = ['Portfolio/templates']
 
 
 # Quick-start development settings - unsuitable for production
