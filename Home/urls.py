07e560267a525f5b45936459914227c3cacc3341
Henry1886 <henryochieng8@gmail.com>
2021-02-11 19:07:08 +0300

    project updated

--- Home/urls.py
+++ Home/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, reverse
 #from . import HomeView
-from django.views.generic import TemplateView
+#from django.views.generic import TemplateView
 
 from .import views
 
 urlpatterns = [
-    #path('', views.Home, name="Home"),
-    path('', TemplateView.as_view(template_name = "Home/index.html")),
+    path('', views.Home, name="Home"),
+    #path('', TemplateView.as_view(template_name = "Home/index.html")),
     #path('About', TemplateView.as_view(template_name = "About/index.html")),
     path('About', views.About, name="About"),
     path('The_Portfolio', views.The_Portfolio, name="The_Portfolio"),
