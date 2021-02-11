13d5f838919f8caf080e7601473f2a2731fac53e
Henry1886 <henryochieng8@gmail.com>
2021-02-11 20:49:30 +0300

    project updated

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -71,7 +71,7 @@ ROOT_URLCONF = 'Portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'Home/templates/Home/index.html')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
