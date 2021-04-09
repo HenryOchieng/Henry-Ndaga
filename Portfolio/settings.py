32fcc1210aaec8d6c5665cf866fbab724ac7346e
Henry1886 <henryochieng8@gmail.com>
2021-04-09 13:20:03 +0300

    changes to the project

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['henry-ndaga.herokuapp.com', '127.0.0.1']
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'henryochieng8@gmail.com'
-EMAIL_HOST_PASSWORD = 'unbeaten49'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
