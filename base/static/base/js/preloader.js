06a62c065a4d17602dcc2f2248e9c7cbbd08b59d
Henry1886 <henryochieng8@gmail.com>
2021-04-15 17:42:56 +0300

    preloader added

---
+++ base/static/base/js/preloader.js
@@ -0,0 +1,7 @@
+var loader = document.getElementById('preloader');
+
+window.addEventListener ("load", function() {
+    //Hide the spinner after  seconds
+setTimeout(function(){loader.style.display = 'none';}, 3000);
+
+});
