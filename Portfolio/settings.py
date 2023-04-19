193f5ca4bfd6ceb0a12017a8611bd3c689664c2d
Henry <henryochieng8@gmail.com>
2023-04-19 11:16:44 +0300

    project changes

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_s3_sqlite',
    # 'crispy_forms',
 ]
 #CRSIPY_TEMPLATE_PACK = 'bootstrap4'
